43d4880a94f24c434af38bbb7895a37d0468fd24
hengsomnang098 <hsomnang098@gmail.com>
2025-10-11 20:02:12 +0700

    setup dockerfile + docker-compose to run all service in docker

--- src/AuctionService/Data/Migrations/AuctionDbContextModelSnapshot.cs
+++ src/AuctionService/Data/Migrations/AuctionDbContextModelSnapshot.cs
@@ -4,7 +4,6 @@ using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -10,14 +10,14 @@ namespace IdentityService.Pages.Diagnostics;
 public class Index : PageModel
 {
     public ViewModel View { get; set; } = default!;
-
+    
     public async Task<IActionResult> OnGet()
     {
         //Replace with an authorization policy check
-        if (HttpContext.Connection.IsRemote())
-        {
-            return NotFound();
-        }
+        // if (HttpContext.Connection.IsRemote())
+        // {
+        //     return NotFound();
+        // }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
 
